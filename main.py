31e4e9b253e1a5c57faf8e9262ffe28d75d00a86
AQUAMXRINE <59778433+AQUAMXRINE@users.noreply.github.com>
2022-10-05 11:09:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import subprocess
 import os
 from time import time, sleep
+from pushnotifier import PushNotifier as pn
 
-folder = ''
+test = True
+folder = './toPrint'
 
 print('''
  /$$$$$$$$        /$$       /$$$$$$$           /$$             /$$    
@@ -15,20 +17,55 @@ print('''
 |__/    \______/ |__/  \__/|__/     |__/      |__/|__/  |__/   \___/                                                                                                         
 ''')
 
-print("Bienvenue sur le service d'impression de PDF by JA !")
-print(f"Chaque PDF fourni dans le dossier '{folder}' sera imprimé sur l'imprimante par défaut (macOS) puis supprimé du dossier.")
+print("Bienvenue sur le service d'impression de PDF par JA !")
+print(f"Chaque PDF fourni dans le dossier '{folder}' sera imprimé sur l'imprimante par défaut (macOS) puis supprimé du dossier.\n")
+
+def crash(arg):
+    print('''\n
+ ,adPPYba, 8b,dPPYba, 8b,dPPYba,  ,adPPYba,  8b,dPPYba,  
+a8P_____88 88P'   "Y8 88P'   "Y8 a8"     "8a 88P'   "Y8  
+8PP""""""" 88         88         8b       d8 88          
+"8b,   ,aa 88         88         "8a,   ,a8" 88          
+ `"Ybbd8"' 88         88          `"YbbdP"'  88          
+
+    ''')
+    print(f'Erreur fatale ! Raison : {arg}.\nArrêt du service.')
+    usernamepn = ''
+    passwordpn = ''
+    pnsession = pn.PushNotifier(usernamepn, passwordpn, 'notiflvs.aqua', '6C3VDE7DV75BDE7D696VDE7D8B2D6C3VKFTTFBTTFT')
+    pnsession.send_text(f"FokPrint s'est arrêté. Raison : {arg}")
+    exit()
+
 def imprimer(file, filename):
     print(f"Impression de {filename} !")
-    printer = subprocess.getoutput("lpstat -d").split(": ")[1]
-    print_cmd = 'lpr -P %s %s'
-    os.system(print_cmd % (printer, file))
+    if not test:
+        try:
+            printer = subprocess.getoutput("lpstat -d").split(": ")[1]
+            print_cmd = 'lpr -P %s %s'
+            os.system(print_cmd % (printer, file))
+        except:
+            crash("Impossible d'imprimer le fichier.")
+    try:
+        os.remove(file)
+        print(f'Le fichier {filename} a bien été supprimé !\n')
+    except:
+        crash(f'Erreur : Impossible de supprimer le fichier {file}, peut-être est-ce dû à un manque de permissions ?')
+
 
 while True:
-    sleep(60 - time() % 60)
     for file in os.listdir(folder):
         f = os.path.join(folder, file)
         if os.path.isfile(f):
-            filename = str(f[44:])
+            filename = str(f[len(folder)+1:])
             if f[-4:] == '.pdf':
                 imprimer(f, filename)
-                os.remove(f)
+            else:
+                try:
+                    os.remove(f)
+                    print(f"Fichier non accepté trouvé. Suppression de {filename}.\n")
+                except:
+                    crash(f"Erreur de suppression d'un fichier non voulu. ({filename})")
+    try:
+        sleep(60 - time() % 60)
+    except KeyboardInterrupt:
+        crash('Arrêt volontaire avec ctrl+c')
